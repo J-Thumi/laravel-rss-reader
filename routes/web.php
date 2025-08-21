5c15ab6790e9fc695ac22377a7bd2099e8d21e3b
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2025-08-21 12:20:01 +0300

    feat: add google and techcrunch news routes

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,9 @@
 <?php
 
+use App\Http\Controllers\rssFeeds;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    // Fetch RSS feed (example: TechCrunch)
-    $response = Http::get('https://techcrunch.com/feed/');
+Route::get('/techcrunch', [rssFeeds::class,'showNews'])->name('techcrunch');
+Route::get('/google', [rssFeeds::class,'showGoogleNews'])->name('google-news');
 
-    // Convert RSS (XML) to SimpleXMLElement
-    $rss = simplexml_load_string($response->body());
-
-    // Convert to array of items
-    $items = [];
-    foreach ($rss->channel->item as $item) {
-        $items[] = [
-            'title' => (string) $item->title,
-            'link'  => (string) $item->link,
-            'date'  => (string) $item->pubDate,
-            'desc'  => (string) $item->description,
-        ];
-    }
-
-    return view('rss', compact('items'));
-});
